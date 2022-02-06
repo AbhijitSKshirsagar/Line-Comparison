717f33ab92b8bc4ee6cce434b320445c544e4fb7
AbhijitSKshirsagar <abhijitkshirsagar793@gmail.com>
2022-02-07 03:09:53 +0530

    added: line_comparison

--- src/line_comperison/Line.java
+++ src/line_comperison/Line.java
@@ -18,7 +18,7 @@ public class Line {
      int y1 = sc.nextInt();
      int y2 = sc.nextInt();
      Double dis1 = Math.sqrt((x2 - x1) * (x2 - x1)  +  (y2 - y1) * (y2 - y1)); 
-     System.out.println("Length of line of coordinates"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis1); 
+     System.out.println("Length of dis of coordinates"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis1); 
        System.out.println("Enter the value of x3 and x4"); 
 	 int x3 = sc.nextInt();
      int x4 = sc.nextInt();
@@ -27,16 +27,16 @@ public class Line {
      int y4 = sc.nextInt();
      Double dis2 = Math.sqrt((x4 - x3) * (x4 - x3)  +  (y2 - y1) * (y2 - y1));
      
-     System.out.println("Length of line of coordinates"+"("+x1+","+x1+"),"+"("+y2+","+y2+")===>"+dis2);  
+     System.out.println("Length of dis of coordinates"+"("+x1+","+x1+"),"+"("+y2+","+y2+")===>"+dis2);  
 	
 	
-	if(dis1 == dis2)
+	if(dis1 > dis2)
       {
-    	  System.out.println("both lines are equal");
+    	  System.out.println("dis1 is greter than dis2");
       }
       else
       {
-    	  System.out.println("both lines are diffrent");
+    	  System.out.println("dis1 is less than dis2");
       }
 	}
 }
